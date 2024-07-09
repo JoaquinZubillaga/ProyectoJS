74da0703f52e42f2d66ca9cecbece8f22df38a8b
Joaquin Zubillaga <joacozubi@gmail.com>
2024-07-08 21:32:32 -0300

    we added two new functions, fahrenheitACelsius and kelvinACelsius

--- js/app.js
+++ js/app.js
@@ -2,11 +2,19 @@
 alert('¡Hola! Bienvenido al convertidor de temperaturas')
 
 function celsiusAFahrenheit(celsius){
-    return celsius * 1.8 + 32;
+    return celsius * 9/5 + 32;
 }
 
 function celsiusAKelvin(celsius){
     return celsius + 273;
+}
+
+function fahrenheitACelsius(fahrenheit){
+    return (fahrenheit -32) * 5/9;
+}
+
+function kelvinACelsius(kelvin){
+    return kelvin -273;
 }
 
 function ajustarDecimales(numero){
@@ -14,7 +22,7 @@ function ajustarDecimales(numero){
 }
 
 function convertirgrados(){
-    let opcion = prompt('Para convertir una unidad en celsius a fahrenheit, ingrese "1"\nPara convertir a kelvin, ingrese "2"')
+    let opcion = prompt('Para convertir una unidad en celsius a fahrenheit, ingrese "1"\nPara convertir una unidad de celsius a kelvin, ingrese "2"\nPara convertir una unidad de fahrenheit a celsius, ingrese "3"\nPara convertir una unidad de kelvin a celsius, ingrese "4"')
     if(opcion === '1'){
         let celsius = parseFloat(prompt('Ingrese la cantidad de grados celsius para convertir en fahrenheit'))
         if(isNaN(celsius)){
@@ -30,6 +38,22 @@ function convertirgrados(){
         } else{
         let kelvin = celsiusAKelvin(celsius)  
         alert(`El equivalente en kelvin de ${ajustarDecimales(celsius)}°C es igual a ${ajustarDecimales(kelvin)}K`)
+        }
+    } else if(opcion === '3'){
+        let fahrenheit = parseFloat(prompt('Ingrese la cantidad de grados fahrenheit para convertir en celsius'))
+        if(isNaN(fahrenheit)){
+            alert('Ingrese un numero válido')
+        } else{
+        let celsius = fahrenheitACelsius(fahrenheit)  
+        alert(`El equivalente en celsius de ${ajustarDecimales(fahrenheit)}°F es igual a ${ajustarDecimales(celsius)}°C`)
+        }
+    } else if(opcion === '4'){
+        let kelvin = parseFloat(prompt('Ingrese la cantidad de grados kelvin para convertir en celsius'))
+        if(isNaN(kelvin)){
+            alert('Ingrese un numero válido')
+        } else{
+        let celsius = kelvinACelsius(kelvin)  
+        alert(`El equivalente en celsius de ${ajustarDecimales(kelvin)}K es igual a ${ajustarDecimales(celsius)}°C`)
         }
     } else{
         alert('No ingresaste ninguna opción correcta')
